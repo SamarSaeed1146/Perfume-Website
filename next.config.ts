b2925da766120b53e8aa9631b10ce50a28559773
SamarSaeed1146 <samarsaeed374@gmail.com>
2025-06-22 16:39:14 +0500

    Add Social Media Reference

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https", // or 'http' if your image URLs start with http
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/a/**", // This path might vary depending on the exact structure, '/a/**' is common for Google profile images. You can start with '/**' if unsure.
+      },
+      // Add other domains here if you use images from other external sources
+      // {
+      //   protocol: 'https',
+      //   hostname: 'example.com',
+      // },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
